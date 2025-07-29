41fc60e83d7b53d07ed1e368a552cfd907789916
yana-burdziuk <yana.burdziuk@gmail.com>
2025-07-29 18:14:23 +0200

    adding new table + testing user creation

---
+++ test.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+require('./models/connection');
+const User = require('./models/users');
+
+const testUser = new User({
+  username: 'testuser',
+  password: 'test123',
+  email: 'test@example.com',
+  totalPoints: 0,
+  totalCo2SavingsPoints: 0,
+  currentPopulation: 0,
+  isAdmin: false
+});
+
+testUser.save().then(() => {
+  console.log('User saved');
+  process.exit();
+});
