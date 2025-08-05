4edf3c00a7f0f44ed38c2596f25779d08c512753
Lionel <lionel.helou@icloud.com>
2025-08-05 16:29:59 +0200

    Modification de l'IP

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ Server running on http://192.168.1.14:${PORT}`);
 });
