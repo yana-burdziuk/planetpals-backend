324449a2426ddf2827fcd8387657e04f5437ca06
Lionel <lionel.helou@icloud.com>
2025-08-30 10:15:02 +0200

    Mise en relation du back et du front pour comments et activity

--- models/connection.js
+++ models/connection.js
@@ -7,4 +7,4 @@ mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
   .catch(error => console.error('MongoDB connection error', error));
 
-module.exports = mongoose;
+console.log(connectionString);
