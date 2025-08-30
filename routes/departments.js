0b9472d1263c8882580f6c9a3b82d085332f8566
yana-burdziuk <yana.burdziuk@gmail.com>
2025-08-30 13:26:26 +0200

    refactoring points calculation and display

--- routes/departments.js
+++ routes/departments.js
@@ -2,6 +2,25 @@ var express = require("express");
 var router = express.Router();
 require("../models/connection"); // lancement de la connexion avec la DB
 const Department = require("../models/departments");
+const User = require("../models/users")
+
+
+// Middleware d'auth
+
+async function authMiddleware(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader)
+    return res.status(401).json({ result: false, error: "No token provided" });
+
+  const token = authHeader.split(" ")[1]; // Bearer <token>
+  const user = await User.findOne({ token });
+  if (!user)
+    return res.status(401).json({ result: false, error: "Invalid token" });
+
+  req.user = user;
+  next();
+}
+
 
 /* POST créer un departement */
 
@@ -57,5 +76,46 @@ router.get("/", async (req, res) => {
     res.status(500).json({result : false, error: "Server error"})
   }
 })
+
+router.get("/department-stats", authMiddleware, async (req, res) => {
+  try {
+    const department = await Department.findById(req.user.departmentId);
+    if (!department) {
+      return res.json({ result: false, error: "Department not found" });
+    }
+    
+    res.json({
+      result: true,
+      stats: {
+        totalCO2: department.totalCo2SavingsPoints,
+        totalPoints: department.totalPoints,
+      }
+    });
+  } catch (error) {
+    res.status(500).json({ result: false, error: error.message });
+  }
+});
+
+// route pour forcer le recalcul de tous les départements
+router.post("/recalculate-departments", async (req, res) => {
+  try {
+    const departments = await Department.find();
+    
+    for (const dept of departments) {
+      const users = await User.find({ departmentId: dept._id });
+      const totalPoints = users.reduce((sum, user) => sum + (user.totalPoints), 0); // on parcourt le tableau users et cumule la valeur de totalPoints de chaque user
+      const totalCO2 = users.reduce((sum, user) => sum + (user.totalCo2SavingsPoints), 0); //sum commence à 0 et on ajoute user.totalPoints à chaque itération
+      
+      await Department.findByIdAndUpdate(dept._id, {
+        totalPoints,
+        totalCo2SavingsPoints: totalCO2
+      });
+    }
+    
+    res.json({ result: true, message: "All departments recalculated" });
+  } catch (error) {
+    res.status(500).json({ result: false, error: error.message });
+  }
+});
 
 module.exports = router;
